adcc4d390f7cc7d0c7efcc7fcdcca36e76a2cba8
ianjhill <ianjhill1@gmail.com>
2022-09-23 12:26:08 -0400

    Fixed HomePage
    Content on HomePage somehow was removed and I've refinished this view

--- Fitness App/Views/HomePageView.swift
+++ Fitness App/Views/HomePageView.swift
@@ -8,21 +8,55 @@
 import SwiftUI
 
 struct HomePageView: View {
+    @FetchRequest(sortDescriptors: []) var workouts: FetchedResults<Workout>
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.date, order: .reverse)]) var food: FetchedResults<Food>
+
     var body: some View {
         NavigationView {
-            VStack(alignment: .center) {
-                // Saved Profile Picture
-                
-                // Past Week's worth of workouts
-                
-                // Past Week's worth of calories eaten
-                
-                
+            VStack(alignment: .leading) {
+                Text("Last 3 workouts completed")
+                    .padding(.horizontal)
+                List {
+                    ForEach(workouts.suffix(3)) { workout in
+                        VStack(alignment: .leading, spacing: 6) { // Line Spacing of 6 between Listed workout type & name
+                            Text(workout.type ?? "Unknown Workout Type")
+                                .font(.headline)
+                            Text(workout.name ?? "Unknown Workout Name")
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                }
+                .listStyle(.plain)
+
+                Text("Last 3 foods eaten")
+                    .padding(.horizontal)
+                List {
+                    ForEach(food.suffix(3)) { food in
+                        HStack {
+                            VStack(alignment: .leading, spacing: 6) { // Line Spacing of 6 between Listed food name & calories
+                                Text(food.name!)
+                                    .bold()
+
+                                Text("\(Int(food.calories))") + Text(" calories")
+                                    .foregroundColor(.red)
+                            }
+                            Spacer()
+                            if let foodDate = food.date {
+                                Text(foodDate.calcTimeSince())
+                                    .foregroundColor(.gray)
+                                    .italic()
+                            }
+                        }
+                    }
+                }
+                .listStyle(.plain)
             }
             .navigationTitle("Ian's Fitness App")
+            .padding()
         }
     }
 }
+
 
 struct HomePageView_Previews: PreviewProvider {
     static var previews: some View {
